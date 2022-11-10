c23cbbe3a7a793beb5f712984b9b2faf30e36eb2
Pooja <poojasai2311@gmail.com>
2022-11-09 19:43:36 -0800

    initial commit

---
+++ blog/2022-11-1-greetings.md
@@ -0,0 +1,18 @@
+---
+slug: greetings
+title: About Me
+authors:
+    - name: Pooja Narasimmarao Padmanaban
+      title: Just a curious AI/ML enthusiast
+      url: https://github.com/PoojaNapa
+      image_url: https://github.com/PoojaNapa.png
+tags: [greetings]
+---
+
+Hi!
+
+I am Pooja and this is my portfolio website. Through this website, I wish to interact, share and discuss my projects and areas of interest with everyone of you.
+
+I would like to welcome you to join me on my "Learn something new everyday" mission. I would like to explore various topics including AI/ML/DL/NLP, coding and other Computer Science. I also like to read and summarize research papers. Apart from these, I am interested in the fields of law and space and I love to watch cricket matches and thriller series/movies and books.
+
+Thanks and hope you had a great read!
---
+++ docusaurus.config.js
@@ -0,0 +1,95 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Welcome to my Portfolio Website',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'PoojaNapa',
+  projectName: 'portfolio_website'
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        blog: {
+          showReadingTime: true,
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: '',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Notes', // courses and notes
+          },
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Projects', // project descriptions and demo?
+          },
+          {to: '/blog', label: 'Blog', position: 'left'}, // Write up about different topics
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'LinkedIn',
+                href: 'https://www.linkedin.com/in/pooja-np/',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/PoojaNapa',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
