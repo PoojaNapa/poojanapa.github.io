3838609984e0487838e2546e0ebf03fbef1e05f3
Pooja <poojasai2311@gmail.com>
2022-11-10 19:09:03 -0800

    added posts

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,8 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   organizationName: 'PoojaNapa',
-  projectName: 'portfolio_website',
+  projectName: 'poojanapa.github.io',
+  deploymentBranch: "gh-pages",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
